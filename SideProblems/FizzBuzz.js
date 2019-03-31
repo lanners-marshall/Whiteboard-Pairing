10893e13d0b7f14228236a769585d313d08e2d5c
lanners-marshall <lanners.marshall@gmail.com>
2019-03-30 17:22:23 -0700

    working on adding side problems that I heard are popular but not listed in files

---
+++ SideProblems/FizzBuzz.js
@@ -0,0 +1,33 @@
+const fizzbuzz = () => {
+  for (let i = 1; i <= 100; i++){
+    if (i % 3 === 0 && i % 5 === 0){
+      console.log('FizzBuzz')
+    } else if (i % 3 === 0){
+      console.log('Fizz')
+    } else if (i % 5 === 0){
+      console.log('Buzz')
+    } else {
+      console.log(i)
+    }
+  }
+}
+
+const fizzbuzzTwo = () => {
+  let i = 1
+  while(i <= 100){
+    if (i % 3 === 0 && i % 5 === 0){
+      console.log('FizzBuzz')
+    } else if (i % 3 === 0){
+      console.log('Fizz')
+    } else if (i % 5 === 0){
+      console.log('Buzz')
+    } else {
+      console.log(i)
+    }
+    i++
+  }
+}
+
+
+fizzbuzz()
+fizzbuzzTwo()
